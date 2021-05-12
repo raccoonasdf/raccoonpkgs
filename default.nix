8c624ec0b136a6ce9361e4b2021412a11ca0a21f
raccoon! <raccoon@raccoon.fun>
2021-05-12 19:47:44 -0400

    Remove rac from rac-vscode-extensions

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 final: prev: {
   base16ify = prev.callPackage ./base16ify {};
   jame = prev.callPackage ./jame {};
-  rac-vscode-extensions = prev.callPackage ./rac-vscode-extensions {};
+  vscode-extensions = prev.callPackage ./vscode-extensions {};
   racmenu = prev.callPackage ./racmenu {};
   racscrot = prev.callPackage ./racscrot {};
 }
