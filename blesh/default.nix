021552c06967ad775a28fe0bdcdf960b40c6b2f4
raccoon! <raccoon@raccoon.fun>
2021-05-17 17:59:08 -0400

    Add ble.sh

---
+++ blesh/default.nix
@@ -0,0 +1,24 @@
+{ lib, stdenv, fetchFromGitHub, git }:
+stdenv.mkDerivation {
+  pname = "blesh";
+  version = "0.3.3";
+
+  src = fetchFromGitHub {
+    owner = "akinomyoga";
+    repo = "ble.sh";
+    rev = "7fa584c42d25f379319fc82c6020b7bcf63f902b";
+    hash = "sha256-Gfo2S1t5Kdy+8TEDS4M5yhyRShvzQIljdE0MQK1CL+4=";
+  };
+
+  nativeBuildInputs = [ git ];
+
+  installPhase = ''
+    make install INSDIR=$out/share/blesh
+  '';
+
+  meta = with lib; {
+    description = "A command line editor written in pure Bash scripts which replaces the default GNU Readline.";
+    license = licenses.bsd3;
+    maintainers = with import ../maintainer-list.nix; [ raccoon ];
+  };
+}
--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 final: prev: {
   rac = {
     base16ify = prev.callPackage ./base16ify {};
+    blesh = prev.callPackage ./blesh {};
     jame = prev.callPackage ./jame {};
     vscode-extensions = prev.callPackage ./vscode-extensions {};
     racmenu = prev.callPackage ./racmenu {};
