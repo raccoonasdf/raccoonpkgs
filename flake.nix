fedfdfe5333c21293990e29a03dcda13df93fb28
raccoon! <raccoon@raccoon.fun>
2021-05-31 16:54:41 -0400

    Provide packages from flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "raccoon's nix packages";
-  outputs = { self }: {
-    overlay = import ./default.nix;
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/release-21.05;
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    systems = [ "x86_64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    overlay = import ./.;
+    packages = forAllSystems (system: ((import ./.) {} nixpkgs.legacyPackages.${system}).rac);
   };
 }
