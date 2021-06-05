c1a14a7c544840fb2cc8634631dc8311dc6925b0
raccoon! <raccoon@raccoon.fun>
2021-06-05 00:21:07 -0400

    Add htop config

---
+++ home/profiles/htop/default.nix
@@ -0,0 +1,13 @@
+{
+  programs.htop = {
+    enable = true;
+
+    settings = with config.lib.htop; {
+      hide_threads = true;
+      
+      fields = with fields; [ PID USER M_RESIDENT IO_RATE COMM ];
+      leftMeters = [ (bar "LeftCPUs2") (bar "Memory") (bar "Swap") ];
+      rightMeters = [ (bar "RightCPUs2") (text "Tasks") (text "LoadAverage") ];
+    };
+  };
+}
