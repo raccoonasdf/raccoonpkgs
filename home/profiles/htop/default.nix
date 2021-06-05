38e506d74e9aa9c44d61b0219f43dd938eda02bc
raccoon! <raccoon@raccoon.fun>
2021-06-05 16:34:38 -0400

    Fix htop meters

--- home/profiles/htop/default.nix
+++ home/profiles/htop/default.nix
@@ -1,13 +1,13 @@
-{
+{ config, ... }: {
   programs.htop = {
     enable = true;
 
-    settings = with config.lib.htop; {
-      hide_threads = true;
-      
-      fields = with fields; [ PID USER M_RESIDENT IO_RATE COMM ];
-      leftMeters = [ (bar "LeftCPUs2") (bar "Memory") (bar "Swap") ];
-      rightMeters = [ (bar "RightCPUs2") (text "Tasks") (text "LoadAverage") ];
-    };
+    settings = with config.lib.htop;
+      {
+        hide_threads = true;
+
+        fields = with fields; [ PID USER M_RESIDENT IO_RATE COMM ];
+      } // leftMeters [ (bar "LeftCPUs2") (bar "Memory") (bar "Swap") ]
+      // rightMeters [ (bar "RightCPUs2") (text "Tasks") (text "LoadAverage") ];
   };
 }
