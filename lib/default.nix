7cfd97e0c49e115806cab15b9fbd999817d9dd1d
raccoon! <raccoon@raccoon.fun>
2021-06-06 00:10:36 -0400

    Rewrite flake.nix Restructure raccoonlib like nixpkgs lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,1 +1,11 @@
-{ maintainers = import ./maintainer-list.nix; }
+lib:
+let
+  raccoonlib = lib.makeExtensible (self:
+    let
+      callLibs = file:
+        import file {
+          inherit lib;
+          raccoonlib = self;
+        };
+    in { maintainers = import ./maintainer-list.nix; });
+in raccoonlib
