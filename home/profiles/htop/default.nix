83e9fd5325aece7a71f8cb510e33f83aa15f020a
raccoon! <raccoon@raccoon.fun>
2021-06-05 20:55:27 -0400

    Add CPU% field to htop

--- home/profiles/htop/default.nix
+++ home/profiles/htop/default.nix
@@ -6,7 +6,7 @@
       {
         hide_threads = true;
 
-        fields = with fields; [ PID USER M_RESIDENT IO_RATE COMM ];
+        fields = with fields; [ PID USER M_RESIDENT PERCENT_CPU IO_RATE COMM ];
       } // leftMeters [ (bar "LeftCPUs2") (bar "Memory") (bar "Swap") ]
       // rightMeters [ (bar "RightCPUs2") (text "Tasks") (text "LoadAverage") ];
   };
