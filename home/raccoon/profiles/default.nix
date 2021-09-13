f32292e768615f6447ca774bd4acab28b62b1d9c
raccoon! <raccoon@raccoon.fun>
2021-09-13 18:40:59 -0400

    Remove emacs from raccoon's profile (it's not presently being used)

--- home/raccoon/profiles/default.nix
+++ home/raccoon/profiles/default.nix
@@ -1,5 +1,5 @@
 { lib, pkgs, config, ... }: {
-  imports = [ ./emacs ./fish ./git ./htop ./kakoune ./ranger ./ssh ];
+  imports = [ ./fish ./git ./htop ./kakoune ./ranger ./ssh ];
 
   home.packages = with pkgs; [ atool fd manix nixfmt ripgrep ];
 
