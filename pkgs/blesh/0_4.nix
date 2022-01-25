611a619fefb92747a1d436cb208a2028b1f3f7d9
raccoon! <raccoon@raccoon.fun>
2022-01-25 14:29:48 -0500

    Update ble.sh Clean out old versions Fixes #1

--- pkgs/blesh/0_4.nix
+++
@@ -1,6 +0,0 @@
-import ./generic.nix {
-  version = "0.4.0-devel2";
-  rev = "276baf24d47ef1df2b000b00461a4e6d9a7b1529";
-  hash = "sha256-UI+b+LOoUpDxNbO2vneWT3v0YdfWoDgeLJmbTqCXzQk=";
-  patches = [ ./0_4.patch ];
-}
