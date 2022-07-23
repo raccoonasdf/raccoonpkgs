54d86a988271b9fb1bd2f8e1f74c405fe2e3eeed
raccoon! <raccoon@raccoon.fun>
2022-07-23 09:29:49 -0400

    Add postgresql-activation module Replace services.postgresql.initialScript with idempotent config

--- nixos/profiles/raccoon.fun/matrix.nix
+++ nixos/profiles/raccoon.fun/matrix.nix
@@ -3,13 +3,15 @@ let config = import ./config.nix;
 in {
   services.postgresql = {
     enable = true;
-    initialScript = pkgs.writeText "synapse-init.sql" ''
-      CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
-      CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
-        TEMPLATE template0
-        LC_COLLATE = "C"
-        LC_CTYPE = "C";
-    '';
+    ensureUsers = [{
+      name = "matrix-synapse";
+      ensurePermissions = { "DATABASE \"matrix-synapse\"" = "ALL PRIVILEGES"; };
+    }];
+  };
+
+  raccoon.postgresql-activation.ensureDatabasesWith = {
+    "matrix-synapse" = ''
+      OWNER = "matrix-synapse" TEMPLATE template0 LC_COLLATE = "C" LC_CTYPE = "C"'';
   };
 
   services.matrix-synapse = {
